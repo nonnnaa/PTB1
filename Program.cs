6ad6ae86146e48469ba12cfbc1e7a5a481ca159c
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-03 15:30:29 +0700

    ptb1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            if(a == 0)
+            {
+                if(b == 0)
+                {
+                    Console.WriteLine("The solution is all x");
+                }
+                else
+                {
+                    Console.WriteLine("No solution");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The solution is : " + (-b / a));
+            }
+        }
+    }
+}
